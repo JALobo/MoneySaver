a78a8f05b27b7ca363dd5ecec1abb2451fef43dc
ThatOneWolfyBoy <joao.andre.lobo@gmail.com>
2022-05-09 23:02:11 +0100

    Alguns ecrâs feitos+

---
+++ app/src/main/java/com/example/moneysaver/ActivityReposicaPpassword.java
@@ -0,0 +1,14 @@
+package com.example.moneysaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityReposicaPpassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reposica_ppassword);
+    }
+}

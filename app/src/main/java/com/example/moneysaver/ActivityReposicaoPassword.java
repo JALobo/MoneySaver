d42a06b7354d43cd2b47c3c7cb2e9845ff0300c8
ThatOneWolfyBoy <joao.andre.lobo@gmail.com>
2022-05-29 15:44:45 +0100

    Objetos User e Despesas feitos

--- app/src/main/java/com/example/moneysaver/ActivityReposicaoPassword.java
+++ app/src/main/java/com/example/moneysaver/ActivityReposicaoPassword.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ActivityReposicaPpassword extends AppCompatActivity {
+public class ActivityReposicaoPassword extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/moneysaver/Expenses.java
@@ -0,0 +1,42 @@
+package com.example.moneysaver;
+
+public class Expenses {
+    private String nameExpense;
+    private String desExpense;
+    private double valExpense;
+
+
+    public Expenses(String nameExpense, String desExpense, double valExpense) {
+        this.nameExpense = nameExpense;
+        this.desExpense = desExpense;
+        this.valExpense = valExpense;
+    }
+
+    //Construtor vazio just because
+    public Expenses() {
+    }
+
+    public String getNameExpense() {
+        return nameExpense;
+    }
+
+    public void setNameExpense(String nameExpense) {
+        this.nameExpense = nameExpense;
+    }
+
+    public String getDesExpense() {
+        return desExpense;
+    }
+
+    public void setDesExpense(String desExpense) {
+        this.desExpense = desExpense;
+    }
+
+    public double getValExpense() {
+        return valExpense;
+    }
+
+    public void setValExpense(double valExpense) {
+        this.valExpense = valExpense;
+    }
+}

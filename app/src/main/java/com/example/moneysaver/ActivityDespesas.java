c5c300e81a5953224e640706de2fa8bf80a533b7
ThatOneWolfyBoy <joao.andre.lobo@gmail.com>
2022-05-29 17:40:05 +0100

    Ligação entre ecrãs realizado. Falta a logica dentro dos ecrâs e a ligação aos fragments

--- app/src/main/java/com/example/moneysaver/ActivityDespesas.java
+++ app/src/main/java/com/example/moneysaver/ActivityDespesas.java
@@ -2,13 +2,52 @@ package com.example.moneysaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ActivityDespesas extends AppCompatActivity {
 
+    Button btnVoltarEditDespesa, btnGerirDespesas;
+    TextView txtTotalInserido, txtTotalDespesas;
+    ListView despesaList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_despesas);
+        btnVoltarEditDespesa=findViewById(R.id.btnVoltarEditDespesa);
+        btnGerirDespesas=findViewById(R.id.btnGerirDespesas);
+
+        //TODO Inserir metodos para popular as TExtViews
+        txtTotalDespesas=findViewById(R.id.txtTotalDespesas);
+        txtTotalInserido=findViewById(R.id.txtTotalInserido);
+
+        //TODO Inserir metodos para popular a lista
+        despesaList.findViewById(R.id.despesaList);
+
+        btnVoltarEditDespesa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                voltar();
+            }
+        });
+
+        btnGerirDespesas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goActivityGerirDespesas();
+            }
+        });
+    }
+    private void goActivityGerirDespesas() {
+        Intent gerirDespesas = new Intent(this, ActivityGerirDespesas.class);
+        startActivity(gerirDespesas);
+    }
+
+    private void voltar() {
+        finish();
     }
 }
--- app/src/main/java/com/example/moneysaver/ActivityTotalMonetario.java
+++ app/src/main/java/com/example/moneysaver/ActivityTotalMonetario.java
@@ -3,12 +3,51 @@ package com.example.moneysaver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityTotalMonetario extends AppCompatActivity {
+
+    Button btnInserir, btnRetirar, btnVoltar;
+    EditText txtValorInserir;
+    TextView txtTotalInserido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_total_monetario);
+
+        btnInserir = findViewById(R.id.btnInserirTM);
+        btnRetirar = findViewById(R.id.btnRetirarTM);
+        btnVoltar = findViewById(R.id.btnVoltar);
+        txtValorInserir = findViewById(R.id.txtValorInserir);
+        txtTotalInserido = findViewById(R.id.txtTotalInserido);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                voltar();
+            }
+        });
+
     }
+
+
+    private void inserirValorMonetario() {
+
+        //inserir valor monetário no programa. Enviar tamebm para a base de dados o valor inserido
+
+    }
+
+    private void retirarValorMonetario() {
+        //Retirar valor monetário no programa. Enviar tamebm para a base de dados o valor retirado
+
+    }
+
+    private void voltar() {
+        finish();
+    }
+
 }
